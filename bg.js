823690b9bc5232034804a78fa562af5d1297bc72
khil94 <khilkhil@naver.com>
2019-08-23 15:22:54 +0900

    first fetch.. but...

---
+++ bg.js
@@ -0,0 +1,23 @@
+const IMG_NUMBER = 4;
+const body = document.querySelector('body');
+
+
+function paintImage(imgNumber){
+    const img = new Image();
+    img.src = `./src/${imgNumber+1}.jpg`;
+    img.classList.add('bgImage');
+    body.appendChild(img);
+    
+}
+
+const genRandom = function(){
+    const number = Math.floor(Math.random()*IMG_NUMBER);
+    return number;
+}
+
+const init = function(){
+    paintImage(genRandom());
+}
+
+
+init();
