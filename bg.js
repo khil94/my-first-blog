c0dff9f06f1f6f6cd232f5bbe0c32ca69269b3d0
khil94 <khilkhil@naver.com>
2019-09-03 13:33:13 +0900

    no blink when you reload the page

--- bg.js
+++ bg.js
@@ -1,22 +1,22 @@
-const IMG_NUMBER = 4;
+// const IMG_NUMBER = 4;
 var body = document.querySelector('body');
 
 
-function paintImage(imgNumber){
+function paintImage(){
     const img = new Image();
-    img.src = `./src/${imgNumber+1}.jpg`;
+    img.src = `./src/4.jpg`;
     img.classList.add('bgImage');
     body.prepend(img);
     
 }
 
-function genRandom(){
-    const number = Math.floor(Math.random()*IMG_NUMBER);
-    return number;
-}
+// function genRandom(){
+//     const number = Math.floor(Math.random()*IMG_NUMBER);
+//     return number;
+// }
 
 function init(){
-    paintImage(genRandom());
+    paintImage();
 }
 
 
--- nav.js
+++ nav.js
@@ -1,19 +1,49 @@
-var navs = document.querySelector('.navItem');
+var navs = document.querySelector('.navItems');
 
 function init(){
-    const testItem = navs.querySelector('.profile');
-    testItem.addEventListener('click',leadToPage);
+    for(var i=0; navs.childNodes[i]!=null ;i++){
+        var child = navs.childNodes[i];
+        if(child.hasChildNodes() && child.firstChild.tagName == 'A' ){
+            child.firstChild.addEventListener('click',leadToPage);
+        }
+    }
+    document.querySelector('.index').addEventListener('click',leadToIndex);
+    refreshPage();
 }
 
 
 function leadToPage(e){
     var name = e.target.className;
 
-    fetch(`./nav/${name}.html`).then(function(response){
-        if(response.status != '404'){
-            console.log(response.status);
-        }else console.log('failed.');
+    fetch(`/nav/${name}.html`)
+    .then((res) =>{
+        if( res.status!=200){
+            return ;
+        }else return res.text();
     })
+    .then((data)=>{
+        if(data!=undefined){
+            document.querySelector('.section').innerHTML = data;
+            console.log(data);
+        }else console.log("Failed to move")
+    });
 }
+
+function leadToIndex(e){
+    var name = e.target.className;
+
+    fetch(`index.html`)
+    .then((res) =>{
+        if( res.status!=200){
+            return ;
+        }else window.location.href = res.url;
+    })
+  
+}
+
+function refreshPage(){
+    fetch('index.css').then((res)=>console.log(res));
+}
+
 
 init();
